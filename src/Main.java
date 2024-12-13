7bf6e0256d0c589419ad28a497ef65fff56243bb
huawei <romanec.aleksey@gmail.com>
2024-12-13 23:24:46 +0400

    HW8.1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,24 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Home Work-8");
+        System.out.println("Задание-1");
+        int[] number = new int[3];
+        number[0] = 1;
+        number[1] = 2;
+        number[2] = 3;
+        System.out.println(number[0]);
+        double[] numberDrobnve = {1.57, 7.654, 9.986};
+
+        System.out.println(numberDrobnve[0]);
+        int[] people = new int[5];
+        people[0] = 10;
+        people[1] = 20;
+        people[2] = 30;
+        people[3] = 40;
+        people[4] = 50;
+        System.out.println(people[4]);
+
+        System.out.println();
 
     }
 }
