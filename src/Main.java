febe33752cffc32cef905d554bb159ae5f9dec0a
huawei <romanec.aleksey@gmail.com>
2024-12-13 23:25:40 +0400

    HW8.1

--- src/Main.java
+++ src/Main.java
@@ -7,9 +7,10 @@ public class Main {
         number[1] = 2;
         number[2] = 3;
         System.out.println(number[0]);
-        double[] numberDrobnve = {1.57, 7.654, 9.986};
 
+        double[] numberDrobnve = {1.57, 7.654, 9.986};
         System.out.println(numberDrobnve[0]);
+
         int[] people = new int[5];
         people[0] = 10;
         people[1] = 20;
